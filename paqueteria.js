a3e324b411e7851a9d25a57f52ead250dae5caa4
BRANDON <bampereza@miumg.edu.gt>
2021-11-14 20:49:19 -0600

    Serie 1

---
+++ paqueteria.js
@@ -0,0 +1,19 @@
+function myfunction() {
+
+   var opcion = document.getElementById("op").value;
+
+
+   switch(opcion){
+
+        case "1":
+         window.location ="Funcion 1.html";
+         break;
+         case "2":
+         window.location ="";
+         break;   
+
+
+   }
+   document.getElementById("demo").innerHTML = text;
+   
+}
